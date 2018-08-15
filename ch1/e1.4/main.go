6b373cb9d2a17405d2fb1359d40456e86d0236c5
Daved <daved@codemodus.com>
2018-08-15 14:55:04 -0700

    Add spacing and remove else from 1.4.

--- ch1/e1.4/main.go
+++ ch1/e1.4/main.go
@@ -15,19 +15,21 @@ type data struct {
 func main() {
 	counts := make(map[string]*data)
 	files := os.Args[1:]
+
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			_ = f.Close()
-		}
 	}
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		defer func() { _ = f.Close() }()
+
+		countLines(f, counts)
+	}
+
 	for line, d := range counts {
 		if d.ct > 1 {
 			fs := strings.Join(d.files, " ")
@@ -40,9 +42,11 @@ func countLines(f *os.File, counts map[string]*data) {
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
 		t := sc.Text()
+
 		if _, ok := counts[t]; !ok {
 			counts[t] = &data{}
 		}
+
 		counts[t].ct++
 		counts[t].files = appendMissing(counts[t].files, f.Name())
 	}
