c33a13cd071d0bbc8ed6b5ab3f5d97d225f6c430
Daved <daved@codemodus.com>
2018-08-15 14:44:51 -0700

    Add exercise 1.4.

---
+++ ch1/e1.4/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type data struct {
+	ct    int
+	files []string
+}
+
+func main() {
+	counts := make(map[string]*data)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			_ = f.Close()
+		}
+	}
+	for line, d := range counts {
+		if d.ct > 1 {
+			fs := strings.Join(d.files, " ")
+			fmt.Printf("%d\t%s\t%s\n", d.ct, line, fs)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]*data) {
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		t := sc.Text()
+		if _, ok := counts[t]; !ok {
+			counts[t] = &data{}
+		}
+		counts[t].ct++
+		counts[t].files = appendMissing(counts[t].files, f.Name())
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+func appendMissing(ss []string, new string) []string {
+	for _, s := range ss {
+		if s == new {
+			return ss
+		}
+	}
+
+	return append(ss, new)
+}
