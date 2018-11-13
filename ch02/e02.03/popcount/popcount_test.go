8aae91d379c9818430688031622205ac8e313275
Daved <daved@codemodus.com>
2018-11-12 18:45:18 -0800

    Add 2.3, 2.4, and 2.5.

---
+++ ch02/e02.03/popcount/popcount_test.go
@@ -0,0 +1,26 @@
+package popcount
+
+import (
+	"testing"
+)
+
+var (
+	x interface{}
+	v = uint64(1 + (1 << 1) + (1 << 2) + (1 << 3) + (1 << 4))
+)
+
+func BenchmarkLoop(b *testing.B) {
+	var n int
+	for i := 0; i < b.N; i++ {
+		n = Loop(v)
+	}
+	x = n
+}
+
+func BenchmarkLong(b *testing.B) {
+	var n int
+	for i := 0; i < b.N; i++ {
+		n = Long(v)
+	}
+	x = n
+}
